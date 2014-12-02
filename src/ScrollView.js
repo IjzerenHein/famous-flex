86d4fd1b083983aafc3f2551312bba62180704c3
IjzerenHein <hrutjes@gmail.com>
2014-12-02 19:12:28 +0100

    Added deprecated ScrollView.js for compatibility reasons for the time being

---
+++ src/ScrollView.js
@@ -0,0 +1,9 @@
+// Deprecated, require FlexScrollView instead
+/*global define, console*/
+/*eslint no-use-before-define:0, no-console:0 */
+define(function(require, exports, module) {
+    if (console.warn) {
+        console.warn('file famous-flex/ScrollView has been deprecated, use famous-flex/FlexScrollView instead');
+    }
+    module.exports = require('./FlexScrollView');
+});
