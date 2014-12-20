887bd4c1b7c9504a4cd2236655d67ed0d5f5858f
Jens Zastrow <jens.zastrow@mj-networks-gmbh.de>
2014-12-20 15:14:02 +0100

    export more classes

--- global-no-famous.template.js
+++ global-no-famous.template.js
@@ -1,9 +1,28 @@
 if (typeof ijzerenhein === 'undefined') {
     ijzerenhein = {};
 }
-_.extend(ijzerenhein, {
-    FlexScrollView: require('./src/FlexScrollView'),
-    layouts: {
-        CoverLayout: require('./src/layouts/CoverLayout')
-    }
-});
+
+ijzerenhein.FlexScrollView = require('./src/FlexScrollView');
+ijzerenhein.FlowLayoutNode = require('./src/FlowLayoutNode');
+ijzerenhein.LayoutContext = require('./src/LayoutContext');
+ijzerenhein.LayoutController = require('./src/LayoutController');
+ijzerenhein.LayoutNode = require('./src/LayoutNode');
+ijzerenhein.LayoutNodeManager = require('./src/LayoutNodeManager');
+ijzerenhein.LayoutUtility = require('./src/LayoutUtility');
+ijzerenhein.ScrollController = require('./src/ScrollController');
+ijzerenhein.ScrollView = require('./src/ScrollView');
+
+ijzerenhein.layout = ijzerenhein.layout || {};
+
+ijzerenhein.layout.CollectionLayout = require('./src/layouts/CollectionLayout');
+ijzerenhein.layout.CoverLayout = require('./src/layouts/CoverLayout');
+ijzerenhein.layout.CubeLayout = require('./src/layouts/CubeLayout');
+ijzerenhein.layout.GridLayout = require('./src/layouts/GridLayout');
+ijzerenhein.layout.HeaderFooterLayout = require('./src/layouts/HeaderFooterLayout');
+ijzerenhein.layout.ListLayout = require('./src/layouts/ListLayout');
+ijzerenhein.layout.NavBarLayout = require('./src/layouts/NavBarLayout');
+ijzerenhein.layout.TableLayout = require('./src/layouts/TableLayout');
+
+ijzerenhein.helpers = ijzerenhein.helpers || {};
+
+ijzerenhein.helpers.LayoutDockHelper = require('./src/helpers/LayoutDockHelper');
