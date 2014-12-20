3f122ac4444965aac39f8efb46370d2093cdf95c
Jens Zastrow <jens.zastrow@mj-networks-gmbh.de>
2014-12-20 15:18:51 +0100

    dont export deprecated ScrollView and TableLayout

--- global-no-famous.template.js
+++ global-no-famous.template.js
@@ -10,7 +10,7 @@ ijzerenhein.LayoutNode = require('./src/LayoutNode');
 ijzerenhein.LayoutNodeManager = require('./src/LayoutNodeManager');
 ijzerenhein.LayoutUtility = require('./src/LayoutUtility');
 ijzerenhein.ScrollController = require('./src/ScrollController');
-ijzerenhein.ScrollView = require('./src/ScrollView');
+//ijzerenhein.ScrollView = require('./src/ScrollView');
 
 ijzerenhein.layout = ijzerenhein.layout || {};
 
@@ -21,7 +21,7 @@ ijzerenhein.layout.GridLayout = require('./src/layouts/GridLayout');
 ijzerenhein.layout.HeaderFooterLayout = require('./src/layouts/HeaderFooterLayout');
 ijzerenhein.layout.ListLayout = require('./src/layouts/ListLayout');
 ijzerenhein.layout.NavBarLayout = require('./src/layouts/NavBarLayout');
-ijzerenhein.layout.TableLayout = require('./src/layouts/TableLayout');
+//ijzerenhein.layout.TableLayout = require('./src/layouts/TableLayout');
 
 ijzerenhein.helpers = ijzerenhein.helpers || {};
 
