eecb7cea37cc94e53dd761dd6e817685520f103a
IjzerenHein <hrutjes@gmail.com>
2015-01-24 00:47:58 +0100

    Preliminary checking new TabBar layout + widget

---
+++ src/layouts/TabBarLayout.js
@@ -0,0 +1,136 @@
+/**
+ * This Source Code is licensed under the MIT license. If a copy of the
+ * MIT-license was not distributed with this file, You can obtain one at:
+ * http://opensource.org/licenses/mit-license.html.
+ *
+ * @author: Hein Rutjes (IjzerenHein)
+ * @license MIT
+ * @copyright Gloey Apps, 2014
+ */
+
+/*global define*/
+
+/**
+ * Tab-bar layout.
+ *
+ * |options|type|description|
+ * |---|---|---|
+ * |`[margins]`|Number/Array|Margins shorthand (e.g. 5, [10, 20], [2, 5, 2, 10])|
+ * |`[spacing]`|Number|Space in between items|
+ * |`[itemSize]`|Number|Width or height of the item (when omitted, the size of the renderable is used)|
+ *
+ * Example:
+ *
+ * ```javascript
+ * var TabBarLayout = require('famous-flex/layouts/TabBarLayout');
+ *
+ * var layout = new LayoutController({
+ *   layout: NavBarLayout,
+ *   layoutOptions: {
+ *     margins: [5, 5, 5, 5], // margins to utilize
+ *     spacing: 10            // space in between items
+ *   },
+ *   dataSource: {
+ *     background: new Surface({properties: {backgroundColor: 'black'}}),
+ *     title: new Surface({content: 'My title'}),
+ *     leftItems:[
+ *       new Surface({
+ *         content: 'left1',
+ *         size: [100, undefined] // use fixed width
+ *       })
+ *     ],
+ *     rightItems: [
+ *       new Surface({
+ *         content: 'right1',
+ *         size: [true, undefined] // use actual width of DOM-node
+ *       }),
+ *       new Surface({
+ *         content: 'right2'
+ *         size: [true, undefined] // use actual width of DOM-node
+ *       })
+ *     ]
+ *   }
+ * });
+ * ```
+ * @module
+ */
+define(function(require, exports, module) {
+
+    // import dependencies
+    var Utility = require('famous/utilities/Utility');
+    var LayoutUtility = require('../LayoutUtility');
+
+    // Define capabilities of this layout function
+    var capabilities = {
+        sequence: true,
+        direction: [Utility.Direction.X, Utility.Direction.Y],
+        trueSize: true
+    };
+
+    // global data
+    var size;
+    var direction;
+    var revDirection;
+    var items;
+    var margins;
+    var spacing;
+    var sizeLeft;
+    var set = {
+        size: [0, 0],
+        translate: [0, 0, 0]
+    };
+    var nodeSize;
+    var offset;
+
+    // Layout function
+    function NavBarLayout(context, options) {
+
+        // Prepare data
+        size = context.size;
+        direction = context.direction;
+        revDirection = direction ? 0 : 1;
+        spacing = context.spacing || 0;
+        items = context.get('items');
+        margins = LayoutUtility.normalizeMargins(options.margins);
+        set.size[0] = context.size[0];
+        set.size[1] = context.size[1];
+        set.size[revDirection] -= (margins[1 - revDirection] + margins[3 - revDirection]);
+        set.translate[0] = 0;
+        set.translate[1] = 0;
+        set.translate[2] = 0;
+        set.translate[revDirection] = margins[direction ? 3 : 0];
+
+        // When no item-size specified, spread all items
+        // out equally over the full width/height, taking into
+        // account margins & spacing
+        if (options.itemSize === undefined) {
+            offset = direction ? margins[0] : margins[3];
+            sizeLeft = size[direction] - (offset + (direction ? margins[2] : margins[1]));
+            sizeLeft -= ((items.length - 1) * spacing);
+            for (var i = 0; i < items.length; i++) {
+
+                // Position item
+                nodeSize = Math.round(sizeLeft / (items.length - i));
+                set.size[direction] = nodeSize;
+                set.translate[direction] = offset;
+                context.set(items[i], set);
+
+                if (i === options.selectedItemIndex) {
+                    context.set('selectedItemOverlay', set);
+                }
+
+                // Prepare for next item
+                offset += nodeSize + spacing;
+                sizeLeft -= (nodeSize + spacing);
+            }
+        }
+        else {
+            // TODO
+        }
+    }
+
+    NavBarLayout.Capabilities = capabilities;
+    NavBarLayout.Name = 'TabBarLayout';
+    NavBarLayout.Description = 'TabBar widget layout';
+    module.exports = NavBarLayout;
+});
---
+++ src/widgets/TabBar.js
@@ -0,0 +1,184 @@
+/**
+ * This Source Code is licensed under the MIT license. If a copy of the
+ * MIT-license was not distributed with this file, You can obtain one at:
+ * http://opensource.org/licenses/mit-license.html.
+ *
+ * @author: Hein Rutjes (IjzerenHein)
+ * @license MIT
+ * @copyright Gloey Apps, 2015
+ */
+
+/*global define, console*/
+/*eslint no-use-before-define:0, no-console:0 */
+
+/**
+ * TabBar widget for famo.us.
+ *
+ * Example:
+ *
+ * ```javascript
+ * var TabBar = require('famous-flex/widgets/TabBar');
+ *
+ * var tabBar = new TabBar({
+ *   layoutOptions: {
+ *   });
+ * });
+ * tabBar.setTabs([
+ * ]);
+ * this.add(tabBar); // add to the render-tree
+ *
+ * tabBar.on('tabchange', function(event) {
+ *   console.log('new tab selected');
+ * });
+ * ```
+ * @module
+ */
+define(function(require, exports, module) {
+
+    // import dependencies
+    var View = require('famous/core/View');
+    var LayoutController = require('../LayoutController');
+    var TabBarLayout = require('../layouts/TabBarLayout');
+
+    /**
+     * @class
+     * @extends View
+     * @param {Object} options Configurable options.
+     * @param {Object} [options.tabBarLayout] Layout-options that are passed to the TabBarLayout.
+     * @param {Array} [items] Tab item renderables (see `setItems`).
+     * @alias module:TabBar
+     */
+    function TabBar(options) {
+        View.apply(this, arguments);
+
+        this._dataSource = {
+            items: [],
+            selectedItemOverlay: undefined
+        };
+        this._selectedItemIndex = 0;
+
+        _createLayout.call(this);
+        _updateDataSource.call(this);
+
+        this.setOptions(this.options);
+    }
+    TabBar.prototype = Object.create(View.prototype);
+    TabBar.prototype.constructor = TabBar;
+
+    TabBar.DEFAULT_OPTIONS = {
+        tabBarLayout: {
+            margins: [0, 0, 0, 0],
+            spacing: 0,
+            selectedItemIndex: 0
+        }
+    };
+
+    function _createLayout() {
+        this.layout = new LayoutController({
+            layout: TabBarLayout,
+            layoutOptions: this.options.tabBarLayout,
+            flow: true,
+            reflowOnResize: false
+        });
+        this.add(this.layout);
+    }
+
+    function _setSelectedItem(index) {
+        if (index !== this._selectedItemIndex) {
+            this._selectedItemIndex = index;
+            this.layout.setLayoutOptions({
+                selectedItemIndex: index
+            });
+        }
+    }
+
+    function _updateDataSource() {
+        this.layout.setDataSource(this._dataSource);
+        for (var i = 0; i < this._dataSource.items.length; i++) {
+            var item = this._dataSource.items[i];
+            item.on('click', _setSelectedItem.bind(this, i));
+        }
+    }
+
+    /**
+     * Patches the TabBar instance's options with the passed-in ones.
+     *
+     * @param {Object} options Configurable options.
+     * @param {Object} [options.tabBarLayout] Layout-options that are passed to the TabBarLayout.
+     * @return {TabBar} this
+     */
+    TabBar.prototype.setOptions = function(options) {
+        View.prototype.setOptions.call(this, options);
+        if (!this.layout) {
+            return this;
+        }
+        if (options.tabBarLayout !== undefined) {
+            this.layout.setLayoutOptions(options.tabBarLayout);
+        }
+        return this;
+    };
+
+    /**
+     * Sets the items for the tab-bar.
+     *
+     * @param {Array} items Array of tab-item renderables.
+     * @return {TabBar} this
+     */
+    TabBar.prototype.setItems = function(items) {
+        this._dataSource.items = items;
+        _updateDataSource.call(this);
+        return this;
+    };
+
+    /**
+     * Get the tab-item renderables for the tab-bar.
+     *
+     * @return {Array} tab-item renderables
+     */
+    TabBar.prototype.getItems = function() {
+        return this._dataSource.items;
+    };
+
+    /**
+     * Sets the items for the tab-bar.
+     *
+     * @param {Renderable} selectedItemOverlay Array of tab-item renderables.
+     * @return {TabBar} this
+     */
+    TabBar.prototype.setSelectedItemOverlay = function(selectedItemOverlay) {
+        this._dataSource.selectedItemOverlay = selectedItemOverlay;
+        _updateDataSource.call(this);
+        return this;
+    };
+
+    /**
+     * Get the tab-item renderables for the tab-bar.
+     *
+     * @return {Array} tab-item renderables
+     */
+    TabBar.prototype.getSelectedItemOverlay = function() {
+        return this._dataSource.selectedItemOverlay;
+    };
+
+    /**
+     * Sets the tabs for the tab-bar.
+     *
+     * @param {Number} index Array of tab-renderables.
+     * @return {TabBar} this
+     */
+    TabBar.prototype.setSelectedItemIndex = function(index) {
+        _setSelectedItem(index);
+        return this;
+    };
+
+    /**
+     * Get the tabs for the tab-bar.
+     *
+     * @return {Array} tabs
+     */
+    TabBar.prototype.getSelectedItemIndex = function() {
+        return this._selectedItemIndex;
+    };
+
+    module.exports = TabBar;
+});
