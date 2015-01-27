c54617314d8be2026850d494f0ecf2dd9e074afb
IjzerenHein <hrutjes@gmail.com>
2015-01-27 15:55:07 +0100

    Work in progress TabBar widget (trying to make it generic and a template for other widgets)

--- src/widgets/TabBar.js
+++ src/widgets/TabBar.js
@@ -36,6 +36,7 @@
 define(function(require, exports, module) {
 
     // import dependencies
+    var Surface = require('famous/core/Surface');
     var View = require('famous/core/View');
     var LayoutController = require('../LayoutController');
     var TabBarLayout = require('../layouts/TabBarLayout');
@@ -45,25 +46,25 @@ define(function(require, exports, module) {
      * @extends View
      * @param {Object} options Configurable options.
      * @param {Object} [options.tabBarLayout] Layout-options that are passed to the TabBarLayout.
-     * @param {Array} [items] Tab item renderables (see `setItems`).
+     * @param {Object} [options.layoutController] Options that are passed to the underlying layout-controller.
      * @alias module:TabBar
      */
     function TabBar(options) {
         View.apply(this, arguments);
 
-        this._dataSource = {
-            items: [],
-            selectedItemOverlay: undefined
-        };
-        this._selectedItemIndex = 0;
+        this._selectedItemIndex = -1;
+        if (options && options.classes) {
+            this.classes = this.classes.concat(options.classes);
+        }
 
         _createLayout.call(this);
-        _updateDataSource.call(this);
+        _createRenderables.call(this);
 
         this.setOptions(this.options);
     }
     TabBar.prototype = Object.create(View.prototype);
     TabBar.prototype.constructor = TabBar;
+    TabBar.prototype.classes = ['ff-widget', 'ff-tabbar'];
 
     TabBar.DEFAULT_OPTIONS = {
         tabBarLayout: {
@@ -89,22 +90,69 @@ define(function(require, exports, module) {
         this.layout.pipe(this._eventOutput);
     }
 
+    function _createRenderables() {
+        this.renderables = {
+            items: [],
+            background: this.createRenderable('background'),
+            selectedItemOverlay: this.  createRenderable('selectedItemOverlay')
+        };
+        this.layout.setDataSource(this.renderables);
+    }
+
+    /**
+     * Helper function that is called whenever a new item is selected
+     */
     function _setSelectedItem(index) {
         if (index !== this._selectedItemIndex) {
+            var oldIndex = this._selectedItemIndex;
             this._selectedItemIndex = index;
             this.layout.setLayoutOptions({
                 selectedItemIndex: index
             });
+            var item = this.renderables.items[index];
+            if (oldIndex >= 0) {
+                var oldItem = this.renderables.items[oldIndex];
+                if (oldItem && oldItem.removeClass) {
+                    oldItem.removeClass('selected');
+                }
+            }
+            if (item && item.addClass) {
+                item.addClass('selected');
+            }
+            if (oldIndex >= 0) {
+                this._eventOutput.emit('tabchange', {
+                    target: this,
+                    index: index,
+                    oldIndex: oldIndex,
+                    item: item
+                });
+            }
         }
     }
 
-    function _updateDataSource() {
-        this.layout.setDataSource(this._dataSource);
-        for (var i = 0; i < this._dataSource.items.length; i++) {
-            var item = this._dataSource.items[i];
-            item.on('click', _setSelectedItem.bind(this, i));
+    /**
+     * Creates a new renderable for the given renderable-id.
+     *
+     * @param {String} id id of the renderable to create.
+     * @param {String|Object|Renderable} [data] data-content passed to the renderable.
+     * @return {TabBar} this
+     */
+    TabBar.prototype.createRenderable = function(id, data) {
+        if ((data !== undefined) && (data instanceof Function)) {
+            return data;
         }
-    }
+        var surface = new Surface({
+            classes: this.classes,
+            content: data ? ('<div>' + data + '</div>') : undefined
+        });
+        surface.addClass(id);
+        if (id === 'item') {
+            if (this.options.size && ((this.options.size[0] === true) || (this.options.size[1] === true))) {
+                surface.setSize(this.options.size);
+            }
+        }
+        return surface;
+    };
 
     /**
      * Patches the TabBar instance's options with the passed-in ones.
@@ -125,14 +173,48 @@ define(function(require, exports, module) {
     };
 
     /**
+     * Sets the renderable with the given id.
+     *
+     * @param {String} id id of the renderable
+     * @param {Renderable} renderable Renderable to update.
+     * @return {TabBar} this
+     */
+    TabBar.prototype.setRenderable = function(id, renderable) {
+        this.renderables[id] = renderable;
+        this.layout.setDataSource(this.renderables);
+        return this;
+    };
+
+    /**
+     * Get the renderable with the given id.
+     *
+     * @return {Renderable} Background renderable.
+     */
+    TabBar.prototype.getRenderable = function(id) {
+        return this.renderables[id];
+    };
+
+    /**
      * Sets the items for the tab-bar.
      *
      * @param {Array} items Array of tab-item renderables.
      * @return {TabBar} this
      */
     TabBar.prototype.setItems = function(items) {
-        this._dataSource.items = items;
-        _updateDataSource.call(this);
+        var currentIndex = this._selectedItemIndex;
+        this._selectedItemIndex = -1;
+        this.renderables.items = [];
+        if (items) {
+            for (var i = 0; i < items.length; i++) {
+                var renderable = this.createRenderable('item', items[i]);
+                renderable.on('click', _setSelectedItem.bind(this, i));
+                this.renderables.items.push(renderable);
+            }
+        }
+        this.layout.setDataSource(this.renderables);
+        if (this.renderables.items.length) {
+            _setSelectedItem.call(this, Math.max(Math.min(currentIndex, this.renderables.items.length - 1), 0));
+        }
         return this;
     };
 
@@ -142,49 +224,7 @@ define(function(require, exports, module) {
      * @return {Array} tab-item renderables
      */
     TabBar.prototype.getItems = function() {
-        return this._dataSource.items;
-    };
-
-    /**
-     * Sets the items for the tab-bar.
-     *
-     * @param {Renderable} selectedItemOverlay Array of tab-item renderables.
-     * @return {TabBar} this
-     */
-    TabBar.prototype.setSelectedItemOverlay = function(selectedItemOverlay) {
-        this._dataSource.selectedItemOverlay = selectedItemOverlay;
-        _updateDataSource.call(this);
-        return this;
-    };
-
-    /**
-     * Get the tab-item renderables for the tab-bar.
-     *
-     * @return {Array} tab-item renderables
-     */
-    TabBar.prototype.getSelectedItemOverlay = function() {
-        return this._dataSource.selectedItemOverlay;
-    };
-
-    /**
-     * Sets the background renderable.
-     *
-     * @param {Renderable} background Background renderable.
-     * @return {TabBar} this
-     */
-    TabBar.prototype.setBackground = function(background) {
-        this._dataSource.background = background;
-        _updateDataSource.call(this);
-        return this;
-    };
-
-    /**
-     * Get the renderable that is shown behind the tabbar items.
-     *
-     * @return {Renderable} Background renderable.
-     */
-    TabBar.prototype.getBackground = function() {
-        return this._dataSource.background;
+        return this.renderables.items;
     };
 
     /**
