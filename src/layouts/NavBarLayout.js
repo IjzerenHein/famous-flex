c63bf4bbd1c0e59b60dcd3805cb951407c09aaa7
IjzerenHein <hrutjes@gmail.com>
2015-06-09 18:19:08 +0200

    Renamed `translateZ` to `zIncrement` for NavBarLayout + fixed z-index for title.

--- src/layouts/NavBarLayout.js
+++ src/layouts/NavBarLayout.js
@@ -17,7 +17,7 @@
  * |options|type|description|
  * |---|---|---|
  * |`[margins]`|Number/Array|Margins shorthand (e.g. 5, [10, 20], [2, 5, 2, 10])|
- * |`[translateZ]`|z-index to use when translating items in front of the background (default: 2)|
+ * |`[zIncrement]`|Z-translation increment used to stack the elements correctly (default: 2)|
  * |`[itemWidth]`|Number|Width of the left & right items|
  * |`[leftItemWidth]`|Number|Width of the left items|
  * |`[rightItemWidth]`|Number|Width of the right items|
@@ -67,7 +67,7 @@ define(function(require, exports, module) {
     module.exports = function NavBarLayout(context, options) {
         var dock = new LayoutDockHelper(context, {
             margins: options.margins,
-            translateZ: options.hasOwnProperty('translateZ') ? options.translateZ : 2
+            translateZ: options.hasOwnProperty('zIncrement') ? options.zIncrement : 2
         });
 
         // Position background
@@ -121,7 +121,7 @@ define(function(require, exports, module) {
             right = Math.min(right, context.size[0] - left);
             context.set(title, {
                 size: [right - left, context.size[1]],
-                translate: [left, 0, 0]
+                translate: [left, 0, dock.get().z]
             });
         }
     };
