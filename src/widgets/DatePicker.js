a8e2574e448d4468df1fa256b22c1c497f7b3484
IjzerenHein <hrutjes@gmail.com>
2016-02-15 09:16:35 +0100

    Fixed incorrect initial values for DatePicker widget (happened when initial values were different from the current system date/time).

--- src/widgets/DatePicker.js
+++ src/widgets/DatePicker.js
@@ -241,9 +241,9 @@ define(function(require, exports, module) {
             var scrollWheel = this.scrollWheels[i];
             var component = scrollWheel.component;
             var item = scrollWheel.scrollController.getFirstVisibleItem();
-            if (item && item.viewSequence) {
-                var viewSequence = item.viewSequence;
-                var renderNode = item.viewSequence.get();
+            var viewSequence = item ? item.viewSequence : scrollWheel.viewSequence;
+            if (viewSequence) {
+                var renderNode = viewSequence.get();
                 var currentValue = component.getComponent(renderNode.date);
                 var destValue = component.getComponent(date);
 
@@ -377,7 +377,7 @@ define(function(require, exports, module) {
             component.createRenderable = _createRenderable.bind(this);
             var viewSequence = new VirtualViewSequence({
                 factory: component,
-                value: component.create(this._date)
+                value: component.create(new Date(this._date.getTime()))
             });
             var options = LayoutUtility.combineOptions(
                 this.options.scrollController, {
